e843ec7843b8c53a5b729d38fd4cea2ffcf08b41
eoc940 <hgk0000@naver.com>
2021-07-23 01:13:10 +0900

    boardController 1차 구현 완료

--- tracycle_project/src/main/java/com/tracycle/recycle/controller/BoardController.java
+++ tracycle_project/src/main/java/com/tracycle/recycle/controller/BoardController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +31,7 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("board")
 @CrossOrigin(origins = "*", maxAge = 6000)
-@Api(tags = {"Tracycle User Controller"})
+@Api(tags = {"Tracycle Board Controller"})
 public class BoardController {
 
 	@Autowired
@@ -92,7 +95,7 @@ public class BoardController {
 	}
 	
 	@ApiOperation(value="게시글을 수정한다", response=BoardVO.class)
-	@PostMapping("updateBoard")
+	@PutMapping("updateBoard")
 	public ResponseEntity<BoardVO> updateBoard(@RequestBody BoardVO board) throws Exception{
 		try {
 			boolean isUpdated = boardService.updateBoard(board);
@@ -103,17 +106,92 @@ public class BoardController {
 			return new ResponseEntity<BoardVO>(HttpStatus.NO_CONTENT);
 		}
 	}
-	/*
-	 * 
+	
 	 
 	@ApiOperation(value="게시글을 삭제한다", response=BoardVO.class)
-	@PostMapping("deleteBoard/{boardId}")
+	@DeleteMapping("deleteBoard/{boardId}")
 	public ResponseEntity<BoardVO> deleteBoard(@PathVariable int boardId) throws Exception {
 		try {
 			boolean isDeleted = boardService.deleteBoard(boardId);
-			
+			if (isDeleted) return new ResponseEntity<BoardVO>(HttpStatus.OK);
+			return new ResponseEntity<BoardVO>(HttpStatus.NO_CONTENT);
+		}catch(RuntimeException e) {
+			e.printStackTrace();
+			return new ResponseEntity<BoardVO>(HttpStatus.NO_CONTENT);
 		}
-	}*/
+	}
+	
+	@ApiOperation(value="모든 게시물을 출력한다", response=List.class)
+	@GetMapping("getAllBoard")
+	public ResponseEntity<List<BoardVO>> getAllBoard() throws Exception {
+		try {
+			List<BoardVO> boardList = boardService.getAllBoard();
+			return new ResponseEntity<List<BoardVO>>(boardList, HttpStatus.OK);
+		}catch(RuntimeException e) {
+			e.printStackTrace();
+			return new ResponseEntity<List<BoardVO>>(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@ApiOperation(value="제목으로 게시물을 검색한다", response=List.class)
+	@GetMapping("findByTitle/{title}")
+	public ResponseEntity<List<BoardVO>> findByTitle(@PathVariable String title) throws Exception {
+		try {
+			List<BoardVO> boardList = boardService.findByTitle(title);
+			return new ResponseEntity<List<BoardVO>>(boardList, HttpStatus.OK);
+		}catch(RuntimeException e) {
+			e.printStackTrace();
+			return new ResponseEntity<List<BoardVO>>(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@ApiOperation(value="아이디로 게시물을 검색한다", response=List.class)
+	@GetMapping("findById/{userId}")
+	public ResponseEntity<List<BoardVO>> findById(@PathVariable String userId) throws Exception {
+		try {
+			List<BoardVO> boardList = boardService.findById(userId);
+			return new ResponseEntity<List<BoardVO>>(boardList, HttpStatus.OK);
+		}catch(RuntimeException e) {
+			e.printStackTrace();
+			return new ResponseEntity<List<BoardVO>>(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@ApiOperation(value="내용으로 게시물을 검색한다", response=List.class)
+	@GetMapping("findById/{content}")
+	public ResponseEntity<List<BoardVO>> findByContent(@PathVariable String content) throws Exception {
+		try {
+			List<BoardVO> boardList = boardService.findByContent(content);
+			return new ResponseEntity<List<BoardVO>>(boardList, HttpStatus.OK);
+		}catch(RuntimeException e) {
+			e.printStackTrace();
+			return new ResponseEntity<List<BoardVO>>(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@ApiOperation(value="지역으로 게시물을 검색한다", response=List.class)
+	@GetMapping("findByArea/{areaId}")
+	public ResponseEntity<List<BoardVO>> findByArea(@PathVariable int areaId) throws Exception {
+		try {
+			List<BoardVO> boardList = boardService.findByArea(areaId);
+			return new ResponseEntity<List<BoardVO>>(boardList, HttpStatus.OK);
+		}catch(RuntimeException e) {
+			e.printStackTrace();
+			return new ResponseEntity<List<BoardVO>>(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@ApiOperation(value="품목으로 게시물을 검색한다", response=List.class)
+	@GetMapping("findByCategory/{categoryId}")
+	public ResponseEntity<List<BoardVO>> findByCategory(@PathVariable int categoryId) throws Exception {
+		try {
+			List<BoardVO> boardList = boardService.findByCategory(categoryId);
+			return new ResponseEntity<List<BoardVO>>(boardList, HttpStatus.OK);
+		}catch(RuntimeException e) {
+			e.printStackTrace();
+			return new ResponseEntity<List<BoardVO>>(HttpStatus.NO_CONTENT);
+		}
+	}
 	
 	
 }
